34bbd462fae9d103d3dd3cc63c1a3c386b40a45b
Willian Tomaz <willianlima28@hotmail.com>
2023-08-06 12:22:26 -0300

    feat(menu) adding new option in main menu (Terraform)

---
+++ projects/devops/terraform/main.sh
@@ -0,0 +1,90 @@
+#!/bin/sh
+
+# ---------------------------------------
+SCRIPT_PATH_TO_FILE=$(readlink -f "$0")
+SCRIPT_CURRENT_PATH=$(dirname "$SCRIPT_PATH_TO_FILE")
+# ---------------------------------------
+
+gum style --foreground "#04B575" '[Menu - DevOps/Terraform]'
+echo ""
+echo '{{ Color "99" "0" " NOTE: " }}' | gum format -t template
+echo '{{ Color "99" "0" " (Important) " }} {{ Italic "To be able to use this module in the CLI, you need to have terraform configured on your machine!" }}' | gum format -t template
+echo ""
+
+MY_CMD_CHOICE=$(gum choose "init" "plan" "apply" "destroy" "workspace" "[exit]")
+
+if [ "$MY_CMD_CHOICE" == "init" ]; then
+    MY_CMD_SUB_CHOICE=$(gum choose "init" "init_reconfigure")
+    
+    gum confirm "
+    Do you want to continue ???
+    " || exit 0
+
+    if [ "$MY_CMD_SUB_CHOICE" == "init_reconfigure" ]; then
+        terraform init -reconfigure
+    else
+        terraform init
+    fi
+
+
+elif [ "$MY_CMD_CHOICE" == "plan" ]; then
+    gum confirm "
+    Do you want to continue ???
+    " || exit 0
+    
+    terraform plan
+
+elif [ "$MY_CMD_CHOICE" == "apply" ]; then
+    MY_CMD_SUB_CHOICE=$(gum choose "apply" "apply_auto_approve")
+
+    gum confirm "
+    Do you want to continue ???
+    " || exit 0
+
+    if [ "$MY_CMD_SUB_CHOICE" == "apply_auto_approve" ]; then
+        terraform apply -auto-approve
+    else
+        terraform apply
+    fi
+
+
+elif [ "$MY_CMD_CHOICE" == "destroy" ]; then
+    MY_CMD_SUB_CHOICE=$(gum choose "destroy" "destroy_auto_approve")
+    
+    gum confirm "
+    Do you want to continue ???
+    " || exit 0
+
+    if [ "$MY_CMD_SUB_CHOICE" == "destroy_auto_approve" ]; then
+        terraform destroy -auto-approve
+    else
+        terraform destroy
+    fi
+
+elif [ "$MY_CMD_CHOICE" == "workspace" ]; then
+    # Show Current Workspace
+    VAR_CURRENT_WORKSPACE=$(terraform workspace show)
+    gum format --type markdown -- "#### INFORMATION ABOUT CURRENT WORKSPACE:" "- $VAR_CURRENT_WORKSPACE"
+    echo "----------------------------------------------------------"
+
+    MY_CMD_SUB_CHOICE=$(gum choose "list" "new" "select" "delete" "[exit]")
+    if [ "$MY_CMD_SUB_CHOICE" == "list" ]; then
+        terraform workspace list
+    elif [ "$MY_CMD_SUB_CHOICE" == "new" ]; then
+        WORKSPACE_NAME=$(gum input --placeholder "Name of the workspace you want to create?")
+        terraform workspace new $WORKSPACE_NAME
+    elif [ "$MY_CMD_SUB_CHOICE" == "select" ]; then
+        MY_TF_LIST_CHOICE=$(terraform workspace list | gum choose)
+        terraform workspace select $MY_TF_LIST_CHOICE
+    elif [ "$MY_CMD_SUB_CHOICE" == "delete" ]; then
+        MY_TF_DELETE_CHOICE=$( | gum choose)
+        terraform workspace delete $MY_TF_DELETE_CHOICE
+    else
+        clear
+        exit 0
+    fi
+
+else
+    clear
+    exit 0
+fi
