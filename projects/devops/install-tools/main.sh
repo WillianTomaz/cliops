785fc4631218b6c289296659c2a661f5849894b1
Willian Tomaz <willianlima28@hotmail.com>
2023-08-19 10:10:13 -0300

    fix(install-tools) Adding clarifying comment

--- projects/devops/install-tools/main.sh
+++ projects/devops/install-tools/main.sh
@@ -6,3 +6,8 @@ SCRIPT_CURRENT_PATH=$(dirname "$SCRIPT_PATH_TO_FILE")
 # ---------------------------------------
 
 gum style --foreground "#04B575" '[Menu - DevOps/Install-Tools]'
+echo ""
+echo '{{ Color "99" "0" " NOTE: " }}' | gum format -t template
+echo '{{ Color "99" "0" " TODO: " }} {{ Italic "Soon will be working on this option" }}' | gum format -t template
+echo ""
+
