27adb465e6227b62e0daa5dcfc4ef98c9c7331eb
Willian Tomaz <willianlima28@hotmail.com>
2023-08-19 11:19:30 -0300

    feat(release-version) Adding new script to check release version

---
+++ get_release_version.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# ---------------------------------------
+SCRIPT_PATH_TO_FILE=$(readlink -f "$0")
+SCRIPT_CURRENT_PATH=$(dirname "$SCRIPT_PATH_TO_FILE")
+# ---------------------------------------
+
+VAR_REPO="WillianTomaz/cliops"
+VAR_LATEST_RELEASE_VERSION_IN_GITHUB=$(curl --silent "https://api.github.com/repos/$VAR_REPO/releases/latest" | grep -Po '"tag_name": "\K.*?(?=")')
+
+
+VAR_CURRENT_VERSION_IN_LOCAL_REPO=$(git tag --sort=committerdate | tail -1)
+
+if [ "$VAR_LATEST_RELEASE_VERSION_IN_GITHUB" == "$VAR_CURRENT_VERSION_IN_LOCAL_REPO" ]; then
+    echo ""
+    echo '{{ Color "99" "0" " RELEASE NOTE: " }} {{ Italic "Everything is up to date!" }}' | gum format -t template
+    echo ""
+else
+    echo ""
+    echo '{{ Color "99" "0" " RELEASE NOTE: " }} {{ Italic "You need to update!" }}' | gum format -t template
+    echo ""
+fi
